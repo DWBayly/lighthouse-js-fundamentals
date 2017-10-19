227bf274bf6ca48c79fb9174d3970f32a1197a4d
dbaylyuvic <dbayly@uvic.ca>
2017-10-18 18:03:50 -0600

    adds looping exercises

---
+++ t.js
@@ -0,0 +1,5 @@
+function ageCalculator(n,bd,cy){
+  return n + " is "+ (cy - bd) + " years old";
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
